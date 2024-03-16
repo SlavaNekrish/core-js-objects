96a4c6a7fea9dbc244fce1978ace937a4be3a1b1
SlavaNekrish <vnekrish@mail.ru>
2024-03-17 01:27:02 +0300

    feat: add 12th task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -196,8 +196,15 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const inst = Object.create(proto);
+
+  Object.keys(obj).forEach((key) => {
+    inst[key] = obj[key];
+  });
+
+  return inst;
 }
 
 /**
